a00beed0504a35bc2367b5ff334941e94ab4b0ae
sarath15 <sarath@navadhiti.com>
2021-03-15 21:42:37 +0530

    changeas

---
+++ src/translate.js
@@ -0,0 +1,44 @@
+import {useTranslation} from 'react-i18next';
+
+function Translate() {
+    const {t, i18n} = useTranslation();
+
+    function handleClick(lang) {
+        i18n.changeLanguage(lang);
+    }
+
+    return (
+        <div className="Translate">
+            <div className="row" className="App-header p-3">
+                <div className="col-md-6">
+                    <h3>Proven-sa : Multilingual Platform</h3>
+                </div>
+                <div className="container">
+                    <header id="translateBtn">
+                        <button type="button" className="btn btn-secondary mr-2"
+                                onClick={() => handleClick('en')}>English
+                        </button>
+                        <button type="button" className="btn btn-secondary mr-2"
+                                onClick={() => handleClick('ko')}>Korien
+                        </button>
+                        <button type="button" className="btn btn-secondary mr-2"
+                                onClick={() => handleClick('ja')}>Japanese
+                        </button>
+                    </header>
+                </div>
+            </div>
+            <div className="container mt-4">
+                <div className="row">
+                    <h3><strong>{t('aboutTitle.1')} :</strong></h3>
+                    <p>{t('content.1')}</p>
+                </div>
+                <div className="row mt2">
+                    <h3><strong>{t('descripTitle.1')} : </strong></h3>
+                    <p>{t('descripInfo.1')}</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Translate;
